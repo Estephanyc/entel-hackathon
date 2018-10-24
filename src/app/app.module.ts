1e450af014dcf34269e59fa2408621861508ef8b
AriellaRogueN <maitilydevidasi@gmail.com>
2018-10-24 16:42:57 -0300

    consulta a lista de equipos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GerencialComponent } from './pages/gerencial/gerencial.component';
@@ -18,7 +19,8 @@ import { SensorComponent } from './elements/sensor/sensor.component';
     SensorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/elements/equipo/equipo.component.ts
+++ src/app/elements/equipo/equipo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-equipo',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipo.component.css']
 })
 export class EquipoComponent implements OnInit {
+  @Input()equipo
 
   constructor() { }
 
