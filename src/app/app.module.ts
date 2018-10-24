b1d16d7bc84f0e7c6cb29e8701bf0b446e1487aa
cyntthiah <cyntthiah@gmail.com>
2018-10-24 11:25:37 -0300

    commit de prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { OperacionalComponent } from './pages/operacional/operacional.component'
 import { ElementsComponent } from './elements/elements.component';
 import { EquipoComponent } from './elements/equipo/equipo.component';
 import { SensorComponent } from './elements/sensor/sensor.component';
+import { HeadersComponent } from './elements/headers/headers.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SensorComponent } from './elements/sensor/sensor.component';
     ElementsComponent,
     EquipoComponent,
     SensorComponent,
+    HeadersComponent,
   ],
   imports: [
     BrowserModule
